4db93182e0fcbec98d7231f133b20d3e5feb2835
quangnguyen <>
2025-06-04 23:16:08 +0700

    no message

--- ios/Classes/TfliteFlutterHelperPlusPlugin.m
+++ ios/Classes/TfliteFlutterHelperPlusPlugin.m
@@ -10,6 +10,6 @@
 
 @implementation TfliteFlutterHelperPlusPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [TfliteFlutterHelperPlusPlugin registerWithRegistrar:registrar];
+  [TfliteFlutterHelperPlusPluginImp registerWithRegistrar:registrar];
 }
 @end
--- ios/Classes/TfliteFlutterHelperPlusPlugin.swift
+++ ios/Classes/TfliteFlutterHelperPlusPlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-public class TfliteFlutterHelperPlusPlugin: NSObject, FlutterPlugin {
+public class TfliteFlutterHelperPlusPluginImp: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "tflite_flutter_helper_plus", binaryMessenger: registrar.messenger())
     let instance = TfliteFlutterHelperPlusPlugin()
